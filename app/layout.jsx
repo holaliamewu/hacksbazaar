8c361bf9e86d9e078d21ee9b632f2477d9d85a54
holaliamewu <me.amewu@gmail.com>
2024-11-05 12:47:47 +0000

    fixed navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { Market, Heart, UserRound } from 'lucide-react';
+import { Newspaper, Heart, UserRound } from 'lucide-react';
 import ComingSoonTag from '@/components/ComingSoonTag'
 import { Manrope } from "next/font/google";
 import "./globals.css";
@@ -12,19 +12,19 @@ export default function RootLayout({ children }) {
 
   const navsData = [
       {
-          title: "Home",
+          title: "home",
           link: "/",
           icon: "Home",
           released: true,
       },
       {
-          title: "Favourites",
+          title: "favourites",
           link: "/favourites",
           icon: "Heart",
           released: false,
       },
       {
-          title: "Profile",
+          title: "profile",
           link: "/profile",
           icon: "UserRound",
           released: true,
@@ -36,14 +36,14 @@ export default function RootLayout({ children }) {
         <body className={manrope.className}>
             <AuthProvider >
                         <span className="fixed left-0 bottom-[5svw] flex items-center justify-center w-screen" >
-                            <nav className="flex justify-between space-x-8 bg-zinc-200 shadow px-8 py-2 rounded-full " >
+                            <nav className="flex justify-between space-x-8 bg-zinc-200 shadow px-8 md:px-12 py-2 rounded-full " >
                                 {navsData.map((nav) => (
                                     <Link 
                                         key={nav.title}
                                         href={nav.released ? nav.link : '/'} 
                                         className='flex flex-col items-center justify-center relative gap-1'>
-                                        {nav.icon === 'Home' ? <Market size='20' stroke-width='1.5' /> : nav.icon === 'Heart' ? <Heart size='22' stroke-width='1.5' /> : nav.icon === 'UserRound' ? <UserRound size='22' stroke-width='1.5' /> : null}
-                                        <span className="text-[12px] ">{nav.title}</span>
+                                        {nav.icon === 'Home' ? <Newspaper size='16' stroke-width='1.4' /> : nav.icon === 'Heart' ? <Heart size='18' stroke-width='1.5' /> : nav.icon === 'UserRound' ? <UserRound size='16' stroke-width='1.5' /> : null}
+                                        <span className="text-[11px] ">{nav.title}</span>
                                         {!nav.released &&  <ComingSoonTag />}
                                     </Link>
                                     ))}
