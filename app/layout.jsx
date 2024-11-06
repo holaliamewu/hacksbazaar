565066cc51d22d88f861927b6fe7ccc03f2f6d8f
holaliamewurancard <emmanuel.amewu@rancard.com>
2024-11-06 01:50:18 +0000

    added current page indicator

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { Newspaper, Heart, UserRound } from 'lucide-react';
-import ComingSoonTag from '@/components/ComingSoonTag'
+import ComingSoonTag from '@/components/ComingSoonTag';
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/lib/shared/contexts/SignupContext";
@@ -9,49 +12,57 @@ import { AuthProvider } from "@/lib/shared/contexts/SignupContext";
 const manrope = Manrope({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname(); 
 
   const navsData = [
-      {
-          title: "home",
-          link: "/",
-          icon: "Home",
-      },
-      {
-          title: "favourites",
-          link: "/favourites",
-          icon: "Heart",
-      },
-      {
-          title: "profile",
-          link: "/profile",
-          icon: "UserRound",
-      },
-  ]
+    {
+      title: "home",
+      link: "/",
+      icon: "Home",
+    },
+    {
+      title: "favourites",
+      link: "/favourites",
+      icon: "Heart",
+    },
+    {
+      title: "profile",
+      link: "/profile",
+      icon: "UserRound",
+    },
+  ];
 
   return (
     <html lang="en">
-        <head >
-            <link rel="icon" type="image/png" sizes="32x32" href="/hacksbazaar-logo-short.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        </head>
-        <body className={manrope.className}>
-            <AuthProvider >
-                        <span className="fixed left-0 bottom-[5svw] flex items-center justify-center w-screen" >
-                            <nav className="flex justify-between space-x-8 bg-zinc-200 shadow px-8 md:px-12 py-2 rounded-full " >
-                                {navsData.map((nav) => (
-                                    <Link 
-                                        key={nav.title}
-                                        href={ nav.link } 
-                                        className='flex flex-col items-center justify-center relative gap-1'>
-                                        {nav.icon === 'Home' ? <Newspaper size='16' stroke-width='1.4' /> : nav.icon === 'Heart' ? <Heart size='18' stroke-width='1.5' /> : nav.icon === 'UserRound' ? <UserRound size='16' stroke-width='1.5' /> : null}
-                                        <span className="text-[11px] ">{nav.title}</span>
-                                    </Link>
-                                    ))}
-                            </nav>
-                        </span>
-                       {children}
-            </AuthProvider>
-        </body>
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/hacksbazaar-logo-short.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
+      <body className={manrope.className}>
+        <AuthProvider>
+          <span className="fixed left-0 bottom-[5svw] flex items-center justify-center w-screen">
+            <nav className="flex justify-between space-x-8 bg-zinc-200 shadow px-8 md:px-12 py-2 rounded-full">
+              {navsData.map((nav) => (
+                <Link 
+                  key={nav.title}
+                  href={nav.link}
+                  className="flex flex-col items-center justify-center relative gap-1"
+                >
+                  {nav.icon === 'Home' ? <Newspaper size='16' strokeWidth='1.4' /> : 
+                   nav.icon === 'Heart' ? <Heart size='18' strokeWidth='1.5' /> : 
+                   nav.icon === 'UserRound' ? <UserRound size='16' strokeWidth='1.5' /> : null}
+                  <span 
+                    className={`text-[11px] ${pathname === nav.link ? 'border-b-2 border-b-black' : ''}`}
+                  >
+                    {nav.title}
+                  </span>
+                </Link>
+              ))}
+            </nav>
+          </span>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
